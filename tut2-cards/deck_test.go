708a990708c0364237aff276cce883ff6cabed20
leefowler <fowler.lee8@gmail.com>
2021-04-14 07:04:46 +0200

    finished cards and started testing

---
+++ tut2-cards/deck_test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestNewDeck(t *testing.T)  {
+	d := newDeck()
+
+	if len(d) != 16 {
+		t.Errorf("Expected deck of length 6, but got %v", len(d))
+	}
+}
