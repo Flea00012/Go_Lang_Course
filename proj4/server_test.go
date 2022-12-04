9a31bbb79406cd051ee3de03e7be8d9e962f2490
fleeB1 <fowler.lee8@gmail.com>
2022-12-04 23:25:20 +0100

    adding server calls into test

--- proj4/server_test.go
+++ proj4/server_test.go
@@ -13,6 +13,10 @@ import (
 )
 
 
+type Handler interface {
+	ServeHTTP(http.ResponseWriter, *http.Request)
+}
+
 func TestSimpleHTTPServer(t *testing.T){
 	srv := &http.Server{
 		Addr: "127.0.0.1:8081",
@@ -43,6 +47,36 @@ func TestSimpleHTTPServer(t *testing.T){
 	}
 	client := new(http.Client)
 	path := fmt.Sprintf("http://%s/", srv.Addr)
+
+	for i, c := range testCases {
+		r, err := http.NewRequest(c.method, path, c.body)
+		if err != nil {
+			t.Errorf("%d: %v", i, err)
+			continue
+		}
+		resp, err := client.Do(r)
+		if err != nil {
+			t.Errorf("%d: %v", i, err)
+			continue
+		}
+		
+		if resp.StatusCode != c.code {
+			t.Errorf("%d: unexpected status code: %q", i, resp.Status)
+		}
+
+		b, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			t.Errorf("%d: %v", i , err)
+			continue
+		}
+		_ = resp.Body.Close()
+
+		if c.response != string(b){
+			t.Errorf("%d: expected %q; actual %q", i, c.response, b)
+		}
+
+	}
 }
+
 
 
