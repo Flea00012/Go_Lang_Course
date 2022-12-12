e23a850e4d92207145955e161c67205326fa279d
fleeB1 <fowler.lee8@gmail.com>
2022-12-12 17:39:28 +0100

    go faster routine

--- proj3/speedy/fast_test.go
+++ proj3/speedy/fast_test.go
@@ -9,7 +9,7 @@ import (
 
 var wg sync.WaitGroup
 var sharedLock sync.Mutex
-var ch chan int
+var num int32
 
 func producerOne(ch chan int) {
 	defer wg.Done()
@@ -17,8 +17,10 @@ func producerOne(ch chan int) {
 	for i := 0; i < 5; i++ {
 		sharedLock.Lock()
 		time.Sleep(1 * time.Nanosecond)
+		num--
 		fmt.Println("producerOne run: ", i)
 		sharedLock.Unlock()
+		ch <- i
 	}
 
 }
@@ -26,23 +28,26 @@ func producerOne(ch chan int) {
 func producerTwo(ch chan int) {
 	defer wg.Done()
 
-	for i := 10; i < 0; i-- {
+	for i := 10; i > 5; i-- {
 		sharedLock.Lock()
 		time.Sleep(2 * time.Nanosecond)
-		fmt.Println("producerOne run: ", i)
-		
+		num++
+		fmt.Println("producerTwo run: ", num)
 		sharedLock.Unlock()
+		ch <- i
 	}
 
 }
 
 func TestXxx(t *testing.T) {
 	fmt.Println("testing of the producer routines")
-
+	ch := make(chan int, 10)
 	wg.Add(2)
 	go producerOne(ch)
 	go producerTwo(ch)
 
-	wg.Wait()
+	fmt.Printf("chan values: %#v\n", ch)
 
+	wg.Wait()
+	close(ch)
 }
