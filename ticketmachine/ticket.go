e4b90421233ca1b51061c94fe4f236f5e92cf01c
fleeB1 <fowler.lee8@gmail.com>
2022-12-14 23:43:28 +0100

    start and block the queue

--- ticketmachine/ticket.go
+++ ticketmachine/ticket.go
@@ -33,13 +33,30 @@ func (q *Queue) New()  {
 			case message = <- q.message:
 				switch message {
 				case messagePassStart:
-					
+					q.waitPass++
+				case messagePassEnd:
+					q.playPass = false
+				case messageTicketStart:
+					q.waitTicket++
+				case messageTicketEnd:
+					q.playTicket = false	
+				}
+				if q.waitPass > 0 && q.waitTicket > 0 && !q.playPass &&!q.playTicket {
+					q.playPass = true
+					q.playTicket = true
+					q.waitTicket--
+					q.waitPass--
+					q.queuePass <- 1
+					q.queueTicket <- 1
 				}
 			}
-
 		}
 	}()
+}
 
+func (q *Queue) StartTicketIssue()  {
+	q.message <- messageTicketStart
+	<- q.queueTicket
 }
 
 func main() {
