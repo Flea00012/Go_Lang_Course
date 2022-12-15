dcd76c24a574625a6a1cd655c5e152cd82465e65
fleeB1 <fowler.lee8@gmail.com>
2022-12-15 21:48:11 +0100

    test 405 code

--- fullapp/backend/app/app_test.go
+++ fullapp/backend/app/app_test.go
@@ -33,3 +33,26 @@ func TestApp(t *testing.T) {
 		t.Errorf("Response should be %s, but got %s ", expected, respString)
 	}
 }
+
+func Test404(t *testing.T) {
+	r := newRouter()
+	mockServer := httptest.NewServer(r)
+	resp, err := http.Post(mockServer.URL + "/hello", "", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if resp.StatusCode != 405 {
+		t.Errorf("Status should be 405, but got %d", resp.StatusCode)
+	}
+	defer resp.Body.Close()
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	respString := string(b)
+	expected := ""
+	if respString != expected {
+		t.Errorf("Response should be %s, got %s instead", expected,respString)
+	}
+}
