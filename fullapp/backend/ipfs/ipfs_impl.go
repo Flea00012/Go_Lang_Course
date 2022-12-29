179d7613c59d8aef1f1fd7597bfb0768cb5873ed
fleeB1 <fowler.lee8@gmail.com>
2022-12-29 11:23:50 +0100

    try main for ipfs - need running node on local

--- fullapp/backend/ipfs/ipfs_impl.go
+++ fullapp/backend/ipfs/ipfs_impl.go
@@ -1,7 +1,7 @@
-package fullapp
+package main
 
 import (
-	"bytes"
+	// "bytes"
 	"fmt"
 	"os"
 	"strings"
@@ -9,37 +9,48 @@ import (
 	shell "github.com/ipfs/go-ipfs-api"
 )
 
-func connectToIPFS() *shell.Shell {
+func main() {
+	// Where your local node is running on localhost:5001
 	sh := shell.NewShell("localhost:5001")
-}
-
-func (sh *shell.Shell) addToIPFS() {
 	cid, err := sh.Add(strings.NewReader("hello world!"))
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "error: %s", err)
-		os.Exit(1)
+        fmt.Fprintf(os.Stderr, "error: %s", err)
+        os.Exit(1)
 	}
-	fmt.Printf("added %s\n", cid)
-	out := fmt.Sprintf("%s.txt", cid)
-	err = sh.Get(cid, out)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "error: %s", err)
-		os.Exit(1)
-	}
+    fmt.Printf("added %s", cid)
 }
 
-func (sh *shell.Shell) getDataFromIPFS() {
-	data, err := sh.Cat(cid)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "error: %s", err)
-		os.Exit(1)
-	}
-	// ...so we convert it to a string by passing it through
-	// a buffer first. A 'costly' but useful process.
-	// https://golangcode.com/convert-io-readcloser-to-a-string/
-	buf := new(bytes.Buffer)
-	buf.ReadFrom(data)
-	newStr := buf.String()
-	fmt.Printf("data %s", newStr)
-}
+// func connectToIPFS() *shell.Shell {
+// 	return shell.NewShell("localhost:5001")
+// }
+
+// func (sh *shell.Shell) addToIPFS() {
+// 	cid, err := s.Add(strings.NewReader("hello world!"))
+// 	if err != nil {
+// 		fmt.Fprintf(os.Stderr, "error: %s", err)
+// 		os.Exit(1)
+// 	}
+// 	fmt.Printf("added %s\n", cid)
+// 	out := fmt.Sprintf("%s.txt", cid)
+// 	err = sh.Get(cid, out)
+// 	if err != nil {
+// 		fmt.Fprintf(os.Stderr, "error: %s", err)
+// 		os.Exit(1)
+// 	}
+// }
+
+// func (sh *shell.Shell) getDataFromIPFS() {
+// 	data, err := sh.Cat(cid)
+// 	if err != nil {
+// 		fmt.Fprintf(os.Stderr, "error: %s", err)
+// 		os.Exit(1)
+// 	}
+// 	// ...so we convert it to a string by passing it through
+// 	// a buffer first. A 'costly' but useful process.
+// 	// https://golangcode.com/convert-io-readcloser-to-a-string/
+// 	buf := new(bytes.Buffer)
+// 	buf.ReadFrom(data)
+// 	newStr := buf.String()
+// 	fmt.Printf("data %s", newStr)
+// }
 
