04857c2a310ef70377e3dc84ec9240b4695ded4e
Lee Fowler <fowler.lee8@gmail.com>
2024-09-24 22:37:31 +0200

    multiple cache writes

--- algos/cache_test.go
+++ algos/cache_test.go
@@ -1,7 +1,32 @@
 package algos
 
-import "testing"
+import (
+	"strconv"
+	"sync"
+	"testing"
 
-func TestCache(t *testing.T)  {
-	
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCache(t *testing.T) {
+	var wg sync.WaitGroup
+	var cache *Cache
+	cache = NewCache()
+
+	for i := 0; i <= 6; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			s := "name-" + strconv.Itoa(i)
+			cache.SetValue(s, "john smith-"+strconv.Itoa(i), 200000000)
+		}()
+	}
+
+	wg.Wait()
+
+	var name string
+	name = cache.GetObject("name-1")
+	assert.Equal(t, "john smith-1", name)
+	name = cache.GetObject("name-6")
+	assert.Equal(t, "john smith-6", name)
 }
